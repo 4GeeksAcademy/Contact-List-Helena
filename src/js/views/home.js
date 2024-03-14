ad0f9dfbd95e763038a8b7172edad99484d4c6cb
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2024-03-14 11:13:20 +0000

    all features working

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,11 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Contact List</h1>
+		<img src="https://cdn-icons-png.flaticon.com/512/2648/2648554.png" alt="agenda" width="600" height="600"></img>
+		
 	</div>
 );
